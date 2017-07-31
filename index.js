21af1f41a614048d588e39ea95c81fadb0f8346d
Rahul Saxena <rahul9iitk@gmail.com>
2017-07-31 18:46:49 +0530

    token added

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ app.post('/webhook/', function (req, res) {
 
 // recommended to inject access tokens as environmental variables, e.g.
 // const token = process.env.FB_PAGE_ACCESS_TOKEN
-const token = "<FB_PAGE_ACCESS_TOKEN>"
+const token = "EAAUHLKtdwNwBALZAGBdODTCJIF2nZCJ9IhhcT1Qf0kzmHPYQPwJhXZAo5toa3m7yAcJfKssatNmPnhXuht8UphkfnuYcnQ85B6qji1yzjZACNyrkbYP2Y3Vrda9lAXliSOC6wZAILxy2GXDYpEa1LRRZAzmVUZBd6IuhUR4p54RPVTvO0ykXTqb";
 
 function sendTextMessage(sender, text) {
 	let messageData = { text:text }
